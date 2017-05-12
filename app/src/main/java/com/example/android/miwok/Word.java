c34efac2929f8c166ea129199f2d6c67ec7c279d
Gregorio <greg.massara@gmail.com>
2017-05-12 16:06:43 +0100

    mImage ID state added to Word class and a getter method

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -10,6 +10,8 @@ public class Word {
     // String values: States of the Object Word
     private String mMiwokTranslation;
     private String mDefaultTraslation;
+    // int value for the resource id of the images
+    private int mImageID;
 
 
     //Constructor to create a Word Object
@@ -30,4 +32,8 @@ public class Word {
         return mDefaultTraslation;
     }
 
+    //get method for the imageID
+    public int getmImageID() {
+        return mImageID;
+    }
 }
